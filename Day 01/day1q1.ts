b9add1c327a0064bb6cde436e1bc63e3377b7044
M Aleem <162026993+Aleem12142@users.noreply.github.com>
2024-04-07 00:40:04 -0700

    Add files via upload

---
+++ Day 01/day1q1.ts
@@ -0,0 +1,2 @@
+let Name: string = "Aleem";
+console.log('Hello', Name, 'would you like learn some typescript today?');
---
+++ Day 01/day1q2.ts
@@ -0,0 +1,4 @@
+let $Name: string = "aleem";
+console.log($Name.toLocaleLowerCase());
+console.log($Name.toLocaleUpperCase());
+console.log($Name.charAt(0).toLocaleUpperCase() + $Name.slice(1).toLocaleLowerCase());
