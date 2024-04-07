94497de7acbb52a8658df66f5b6838256fdcca52
M Aleem <162026993+Aleem12142@users.noreply.github.com>
2024-04-07 00:57:06 -0700

    Add files via upload

---
+++ Day 13/day13q37.ts
@@ -0,0 +1,7 @@
+function $make_shirt(size: string = 'larg', messege: string = 'I like coding'){
+    console.log('Make a', size, 'sirt with the messege', messege, 'written on it.');
+}
+
+$make_shirt();
+$make_shirt('medium');
+$make_shirt('small', 'Dive in coding');
