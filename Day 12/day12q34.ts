93cee454bf3cd5aac8cec5e17d7c261ce614cc35
M Aleem <162026993+Aleem12142@users.noreply.github.com>
2024-04-07 00:55:57 -0700

    Add files via upload

---
+++ Day 12/day12q34.ts
@@ -0,0 +1,6 @@
+let pizzas: string[] = ['Chicken pizza', 'Pizza Margherita', 'Eztra cheez pizza', 'Pepperoni pizza', 'BBQ pizza', 'Hawaiian pizza'];
+
+pizzas.forEach(pizza =>{
+    console.log('I like', pizza, '.');
+});
+console.log('I really love pizza!');
---
+++ Day 12/day12q35.ts
@@ -0,0 +1,6 @@
+let animals: string[] =['cat', 'dog', 'parrot', 'rabbit'];
+
+animals.forEach(animal =>{
+    console.log('A', animal, 'would make a greate pet.');
+});
+console.log('Any of these animals would make a greate pet.');
