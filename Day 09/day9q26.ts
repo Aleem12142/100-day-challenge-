769ff5af39ecfca689a657ace5ba7ef74a3c5be9
M Aleem <162026993+Aleem12142@users.noreply.github.com>
2024-04-07 00:52:42 -0700

    Add files via upload

---
+++ Day 09/day9q26.ts
@@ -0,0 +1,14 @@
+//Version that runs if block
+let $alien_colour = 'green';
+if($alien_colour == 'green'){
+    console.log('You just earn 5 points!');
+}else{
+    console.log('You just earn 10 points!');
+}
+//version tha runs else bolock
+$alien_colour = 'blue';
+if($alien_colour == 'green'){
+    console.log('You just learn 10 points!');
+}else{
+    console.log('You just earn 10 points!');
+}
