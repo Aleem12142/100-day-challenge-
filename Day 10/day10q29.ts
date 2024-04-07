b1133fcdcec7fdd6b074413b6e4380c900bd606e
M Aleem <162026993+Aleem12142@users.noreply.github.com>
2024-04-07 00:53:51 -0700

    Add files via upload

---
+++ Day 10/day10q29.ts
@@ -0,0 +1,31 @@
+//favourite_fruite array
+let favourite_fruite: string[] = ['apple', 'banana', 'cherry', 'mango'];
+
+//Function to check if fruit is in array
+ function checkIfFruiteIsInArray(fruite: string): boolean{
+    return favourite_fruite.indexOf(fruite) !== -1;
+ }
+
+ //Example
+ let fruiteToCheck = 'apple';
+ if(checkIfFruiteIsInArray(fruiteToCheck)){
+    console.log('You really like', fruiteToCheck, '.');
+ }
+
+ //Another example
+ let fruiteToCheck1 = 'banana';
+ if(checkIfFruiteIsInArray(fruiteToCheck1)){
+    console.log('You really like', fruiteToCheck1, '.');
+ }
+
+ //Another example
+ let fruiteToCheck2 = 'cherry';
+ if(checkIfFruiteIsInArray(fruiteToCheck2)){
+    console.log('You really like', fruiteToCheck2, '.');
+ }
+
+ //Another example
+ let fruiteToCheck3 = 'mango';
+ if(checkIfFruiteIsInArray(fruiteToCheck3)){
+    console.log('You really like', fruiteToCheck3, '.');
+ }
